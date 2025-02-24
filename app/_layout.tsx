807955bb5187ea4a6fc7e3e0d9c38a38458c63ba
Dragan Stojanovski <dragan.stojanovski@outlook.com>
2025-02-24 21:38:50 +1100

    added tamagui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,49 @@
 import { Stack } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useReactQueryDevTools } from "@dev-plugins/react-query";
+import "../tamagui-web.css";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useColorScheme } from "react-native";
+import { TamaguiProvider } from "tamagui";
+import { tamaguiConfig } from "../tamagui.config";
+// import { useFonts } from "expo-font";
+// import { useEffect } from "react";
 
 const queryClient = new QueryClient({});
 
 export default function RootLayout() {
   useReactQueryDevTools(queryClient);
 
+  const colorScheme = useColorScheme();
+
+  // const [loaded] = useFonts({
+  //   Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
+  //   InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
+  // });
+
+  // useEffect(() => {
+  //   if (loaded) {
+  //     // can hide splash screen here
+  //   }
+  // }, [loaded]);
+
+  // if (!loaded) {
+  //   return null;
+  // }
+
   return (
     <QueryClientProvider client={queryClient}>
-      <Stack />
+      <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+        <ThemeProvider
+          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+        >
+          <Stack />
+        </ThemeProvider>
+      </TamaguiProvider>
     </QueryClientProvider>
   );
 }
