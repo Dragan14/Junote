e18f1f71947957fb7d549b1a3c45ab2c0c530abd
Dragan Stojanovski <dragan.stojanovski@outlook.com>
2025-02-24 19:56:08 +1100

    added react query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,15 @@
 import { Stack } from "expo-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useReactQueryDevTools } from "@dev-plugins/react-query";
+
+const queryClient = new QueryClient({});
 
 export default function RootLayout() {
-  return <Stack />;
+  useReactQueryDevTools(queryClient);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Stack />
+    </QueryClientProvider>
+  );
 }
