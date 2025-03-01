2651ff09c88b6ebb044f59804d68fb350b09d746
Dragan Stojanovski <dragan.stojanovski@outlook.com>
2025-03-01 17:43:39 +1100

    removed tamagui

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,57 +2,31 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useReactQueryDevTools } from "@dev-plugins/react-query";
-import "../tamagui-web.css";
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
 import { useColorScheme } from "react-native";
-import { TamaguiProvider } from "tamagui";
-import { tamaguiConfig } from "../tamagui.config";
-import { useFonts } from "expo-font";
-import { useEffect } from "react";
-import * as SplashScreen from "expo-splash-screen";
 
 const queryClient = new QueryClient({});
 
 export default function RootLayout() {
   useReactQueryDevTools(queryClient);
-
   const colorScheme = useColorScheme();
-
-  const [loaded] = useFonts({
-    Inter: require("@tamagui/font-inter/otf/Inter-Medium.otf"),
-    InterBold: require("@tamagui/font-inter/otf/Inter-Bold.otf"),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
 
   return (
     <QueryClientProvider client={queryClient}>
-      <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
-        <ThemeProvider
-          value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-        >
-          <Stack>
-            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-            <Stack.Screen
-              name="+not-found"
-              options={{ title: "Oops! Not Found" }}
-            />
-          </Stack>
-          <StatusBar style="light" />
-        </ThemeProvider>
-      </TamaguiProvider>
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="+not-found"
+            options={{ title: "Oops! Not Found" }}
+          />
+        </Stack>
+        <StatusBar style="light" />
+      </ThemeProvider>
     </QueryClientProvider>
   );
 }
