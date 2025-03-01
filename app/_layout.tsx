7b8716aa601297aa252f7ca8e932806177f8c2ee
Dragan Stojanovski <dragan.stojanovski@outlook.com>
2025-03-01 18:06:22 +1100

    added react native paper

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,31 +2,46 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useReactQueryDevTools } from "@dev-plugins/react-query";
-import {
-  DarkTheme,
-  DefaultTheme,
-  ThemeProvider,
-} from "@react-navigation/native";
+// import {
+//   DarkTheme,
+//   DefaultTheme,
+//   ThemeProvider,
+// } from "@react-navigation/native";
 import { useColorScheme } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import {
+  Provider as PaperProvider,
+  MD3DarkTheme,
+  MD3LightTheme,
+} from "react-native-paper";
 
 const queryClient = new QueryClient({});
 
 export default function RootLayout() {
   useReactQueryDevTools(queryClient);
+
   const colorScheme = useColorScheme();
 
+  const paperTheme = colorScheme === "dark" ? MD3DarkTheme : MD3LightTheme;
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen
-            name="+not-found"
-            options={{ title: "Oops! Not Found" }}
-          />
-        </Stack>
-        <StatusBar style="light" />
-      </ThemeProvider>
-    </QueryClientProvider>
+    <SafeAreaProvider>
+      <QueryClientProvider client={queryClient}>
+        <PaperProvider theme={paperTheme}>
+          {/* <ThemeProvider
+            value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
+          > */}
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen
+              name="+not-found"
+              options={{ title: "Oops! Not Found" }}
+            />
+          </Stack>
+          <StatusBar style="light" />
+          {/* </ThemeProvider> */}
+        </PaperProvider>
+      </QueryClientProvider>
+    </SafeAreaProvider>
   );
 }
